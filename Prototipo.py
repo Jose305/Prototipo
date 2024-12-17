4a61df62abbfb2eece6746ce098cbd6d325f2ea8
José Ruiz <jose96ph@gmail.com>
2024-12-16 19:31:15 -0500

    Cambios en el codigo del software

--- Prototipo.py
+++ Prototipo.py
@@ -361,7 +361,6 @@ def run_in_thread(func):
     """
     thread = threading.Thread(target=func)
     thread.start()
-    thread.join()  # Espera a que el hilo termine
 
 def show_message(message):
     """
@@ -477,7 +476,7 @@ frameFooter = customtkinter.CTkFrame(master=root, width=600, height=30)
 frameFooter.grid(row=5, column=0, padx=10, pady=10, sticky="we", columnspan=2)
 
 # Footer
-footer_label = customtkinter.CTkLabel(frameFooter, text="Copyright © José Ruiz 2024\nESPE", font=("Helvetica", 12, "bold"))
+footer_label = customtkinter.CTkLabel(frameFooter, text="Copyright © ESPE 2024\n\nDesarrollado por José Ruiz", font=("Helvetica", 12, "bold"))
 footer_label.grid(row=0, column=0, padx=20, pady=10, sticky="we")
 
 # Ajustar las columnas del frameFooter para centrar el texto
